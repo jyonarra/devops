668c1559c9e8530e8447cadabf543fee8ed873d2
jyonarra <45851433+jyonarra@users.noreply.github.com>
2020-06-09 14:14:42 +0530

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -98,7 +98,7 @@ then
             fi
         else
             ##undeploy app
-            ssh -q ${ENV_USER}@${ENV_HOSTNAME} "cd ${ENV_INSTANCE} && ./jboss-cli.sh --connect --command='undeploy ${ARTIFACT}'"
+            ssh -q ${ENV_USER}@${ENV_HOSTNAME} "cd ${ENV_INSTANCE} && ./jboss-cli.sh --connect controller=169.56.110.155:9990 --command='undeploy ${ARTIFACT}'"
             if [ $? -eq 0 ]
             then
                 echo "Undeployed ${ARTIFACT} successfully on ${ENV_HOSTNAME}"
@@ -109,7 +109,7 @@ then
             fi
 
             ##deploy app
-            ssh -q ${ENV_USER}@${ENV_HOSTNAME} "cd ${ENV_INSTANCE} && ./jboss-cli.sh --connect --command='deploy ${ENV_DEPLOY}/${ARTIFACT} --force'"
+            ssh -q ${ENV_USER}@${ENV_HOSTNAME} "cd ${ENV_INSTANCE} && ./jboss-cli.sh --connect controller=169.56.110.155:9990 --command='deploy ${ENV_DEPLOY}/${ARTIFACT} --force'"
             if [ $? -eq 0 ]
             then
                 echo "Deployed ${ARTIFACT} successfully on ${ENV_HOSTNAME}"
