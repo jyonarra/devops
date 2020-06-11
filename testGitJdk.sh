89fd1b357e69b9300590fa24271c1b155fd12331
jyothin212 <45851433+jyothin212@users.noreply.github.com>
2020-06-11 14:34:33 +0530

    Update testGitJdk.sh

--- testGitJdk.sh
+++ testGitJdk.sh
@@ -15,7 +15,7 @@ git clone https://github.com/jyonarra/sample-poc.git
 echo "CHECKING OUT DEVOPS PROPERTY CODE FROM GIT REPOSITORY"
 cd $WORKSPACE
 rm -rf devops
-git clone -b $App_name https://github.com/jyonarra/devops.git
+git clone -b $App_name https://github.com/jyothin212/devops.git
 cd devops
 mv * ../
 cd $WORKSPACE
