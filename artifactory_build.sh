85729b838a1683370eed28515734204595079519
jyothin212 <45851433+jyothin212@users.noreply.github.com>
2020-06-11 16:04:52 +0530

    Update artifactory_build.sh
    Updated Nexus code

--- artifactory_build.sh
+++ artifactory_build.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
-USRNAME=`grep username artifactory.properties|awk -F"=" '{print $2}'`
-PASSWD=`grep password artifactory.properties|awk -F"=" '{print $2}'`
-APP=`grep app_name artifactory.properties|awk -F"=" '{print $2}'`
-MODULE=`grep module_name artifactory.properties|awk -F"=" '{print $2}'`
-PROJECT=`grep application_name artifactory.properties|awk -F"=" '{print $2}'`
-SERVER=`grep server_name artifactory.properties|awk -F"=" '{print $2}'`
-PORT=`grep port_number artifactory.properties|awk -F"=" '{print $2}'`
-FILE_LOCATION=`grep file_path artifactory.properties|awk -F"=" '{print $2}'`
-
-
+ARTIFACTORY_NAME=`grep artifactory_name artifactory.properties|awk -F"=" '{print $2}'`
+echo "Artifactory is ${ARTIFACTORY_NAME}"
 if [ $# -eq 1 ]
 then
-       curl -u${USRNAME}:${PASSWD} -T ${FILE_LOCATION} "http://${SERVER}:${PORT}/artifactory/generic-local/${APP}/${1}/${MODULE}"
+if [ ${ARTIFACTORY_NAME} == "JFROG" ]
+then
+    USRNAME=`grep ${ARTIFACTORY_NAME}_username artifactory.properties|awk -F"=" '{print $2}'`
+    PASSWD=`grep ${ARTIFACTORY_NAME}_password artifactory.properties|awk -F"=" '{print $2}'`
+    APP=`grep ${ARTIFACTORY_NAME}_app_name artifactory.properties|awk -F"=" '{print $2}'`
+    MODULE=`grep ${ARTIFACTORY_NAME}_module_name artifactory.properties|awk -F"=" '{print $2}'`
+    PROJECT=`grep ${ARTIFACTORY_NAME}_application_name artifactory.properties|awk -F"=" '{print $2}'`
+    SERVER=`grep ${ARTIFACTORY_NAME}_server_name artifactory.properties|awk -F"=" '{print $2}'`
+    PORT=`grep ${ARTIFACTORY_NAME}_port_number artifactory.properties|awk -F"=" '{print $2}'`
+    FILE_LOCATION=`grep file_path artifactory.properties|awk -F"=" '{print $2}'`
+
+      echo "curl -u${USRNAME}:${PASSWD} -T ${FILE_LOCATION} "http://${SERVER}:${PORT}/artifactory/generic-local/${APP}/${1}/${MODULE}""
         if [ $? -eq 0 ]
         then
                 echo "Upload is successful.You can see the uploaded artifact in http://${SERVER}:${PORT}/artifactory/generic-local/${APP}/${1}/"
         else
                 echo "Upload is not successful. Please upload the file again"
-	exit 1
-        fi
+                    exit 1
+       fi
+fi
+
+if [ ${ARTIFACTORY_NAME} == "NEXUS" ]
+then
+    USRNAME=`grep ${ARTIFACTORY_NAME}_username artifactory.properties|awk -F"=" '{print $2}'`
+    PASSWD=`grep ${ARTIFACTORY_NAME}_password artifactory.properties|awk -F"=" '{print $2}'`
+    APP=`grep ${ARTIFACTORY_NAME}_app_name artifactory.properties|awk -F"=" '{print $2}'`
+    MODULE=`grep ${ARTIFACTORY_NAME}_module_name artifactory.properties|awk -F"=" '{print $2}'`
+    PROJECT=`grep ${ARTIFACTORY_NAME}_application_name artifactory.properties|awk -F"=" '{print $2}'`
+    SERVER=`grep ${ARTIFACTORY_NAME}_server_name artifactory.properties|awk -F"=" '{print $2}'`
+    PORT=`grep ${ARTIFACTORY_NAME}_port_number artifactory.properties|awk -F"=" '{print $2}'`
+    FILE_LOCATION=`grep ${ARTIFACTORY_NAME}_file_path artifactory.properties|awk -F"=" '{print $2}'`
+
+        curl -v -u ${USRNAME}:${PASSWD} --upload-file ${FILE_LOCATION} http://${SERVER}:${PORT}/repository/sprint_applications/${APP}/${1}/${MODULE}
+        if [ $? -eq 0 ]
+        then
+                echo "Upload is successful.You can see the uploaded artifact in http://${SERVER}:${PORT}/repository/sprint_applications/${APP}/${1}/"
+        else
+                echo "Upload is not successful. Please upload the file again"
+                    exit 1
+                fi
+fi
 else
-        echo "Please pass argument: <build_number>"
-fi 
+    echo "Please pass argument: <build_number>"
+fi
